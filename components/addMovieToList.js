a5c8bfc3fad99e63edff916ed55583a0ee5a4c88
thatzita <robinudehn@gmail.com>
2018-09-19 10:19:38 +0200

    small changes

--- components/addMovieToList.js
+++ components/addMovieToList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import css from "../static/addMovieStyle.css"
 
 class AddMovieToList extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             name: "",
@@ -11,129 +11,124 @@ class AddMovieToList extends React.Component {
             rating: "",
         }
         this.commonChange = this.commonChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     commonChange(event) {
+
         let arr = [...this.state.genre];
 
-        if(event.target.name === undefined || event.target.name === ""){
+        if (event.target.name === undefined || event.target.name === "") {
             this.setState({
                 rating: event.target.value
             });
-        }else if(event.target.name === "genre"){
-
+        } else if (event.target.name === "genre") {
             arr.push(event.target.value);
-
             this.setState({
                 genre: arr
             });
 
             let newArr = arr;
-            let h,i,j;
+            let h, i, j;
 
-            for(h = 0; h < arr.length; h++) {
-                let curGenre = arr[h];
+            for (h = 0; h < arr.length; h++) {
+                let currentGenre = arr[h];
                 let foundCount = 0;
-                for(i = 0; i < arr.length; i++) {
+                for (i = 0; i < arr.length; i++) {
                     if (arr[i] == arr[h])
                         foundCount++;
                 }
-                if(foundCount > 1) {
-                    for(j = 0; j < newArr.length; j++) {
-                        if(newArr[j] == curGenre) {
+                if (foundCount > 1) {
+                    for (j = 0; j < newArr.length; j++) {
+                        if (newArr[j] == currentGenre) {
                             newArr.splice(j, 1);
                             j--;
                         }
                     }
                 }
             }
-
-        }else{
+        } else {
             this.setState({
                 [event.target.name]: event.target.value
             });
         }
-
     }
 
-    submitMovie(data){
-        console.log(this.state)
-     let postData = {
-        name: this.state.name,
-        genre: [...this.state.genre],
-        year: this.state.year,
-        rating: this.state.rating };
+    submitMovie(data) {
+        let postData = {
+            name: this.state.name,
+            genre: [...this.state.genre],
+            year: this.state.year,
+            rating: this.state.rating
+        };
 
         fetch('http://localhost:3000/addmovie/newmovie', {
             method: 'post',
             body: JSON.stringify(postData),
-          }).then(function(response) {
+        }).then(function (response) {
             return response.json();
-          }).then(function(data) {
-            console.log(data)
-          });
+        }).then(function (data) {
+            console.log(data);
+            document.getElementById("movieForm").reset();
+        });
     }
 
     render() {
-
         return (
-<div>
-
-    <div className={css.addContent}>
-        <div className={css.addContainer}>
             <div>
-                <label className={css.labelClass}>Title</label>
-                <br />
-                <input type="text" id="mTitle" name="name" placeholder="Movie name..." onChange={this.commonChange} />
-            </div>
-            <br />
-            <div>
-                <label className={css.labelClass}>Release Year</label><br />
-                <input type="text" id="mYear" name="year" placeholder="Release year" onChange={this.commonChange} />
-            </div>
-            <br />
-            <div>
-                <label className={css.labelClass}>Genre</label><br />
-                <div className={css.checkboxes} onChange={this.commonChange}>
-                    <input type="checkbox" name="genre" value="action" /> <span className={css.checkboxFont}>Action</span>
-                    <input type="checkbox" name="genre" value="comedy" /> <span className={css.checkboxFont}>Comedy</span>
-                    <input type="checkbox" name="genre" value="horror" /> <span className={css.checkboxFont}>Horror</span><br/>
-                    <input type="checkbox" name="genre" value="drama" /> <span className={css.checkboxFont}>Drama</span>
-                    <input type="checkbox" name="genre" value="thriller" /> <span className={css.checkboxFont}>Thriller</span>
-                    <input type="checkbox" name="genre" value="animated" /> <span className={css.checkboxFont}>Animated</span>
+                <div className={css.addContent}>
+                    <form id="movieForm">
+                        <div className={css.addContainer}>
+                            <div>
+                                <label className={css.labelClass}>Title</label>
+                                <br />
+                                <input type="text" id="mTitle" name="name" placeholder="Movie name..." onChange={this.commonChange} />
+                            </div>
+                            <br />
+                            <div>
+                                <label className={css.labelClass}>Release Year</label><br />
+                                <input type="text" id="mYear" name="year" placeholder="Release year" onChange={this.commonChange} />
+                            </div>
+                            <br />
+                            <div>
+                                <label className={css.labelClass}>Genre</label><br />
+                                <div className={css.checkboxes} onChange={this.commonChange}>
+                                    <input type="checkbox" name="genre" value="action" /> <span className={css.checkboxFont}>Action</span>
+                                    <input type="checkbox" name="genre" value="comedy" /> <span className={css.checkboxFont}>Comedy</span>
+                                    <input type="checkbox" name="genre" value="horror" /> <span className={css.checkboxFont}>Horror</span><br />
+                                    <input type="checkbox" name="genre" value="drama" /> <span className={css.checkboxFont}>Drama</span>
+                                    <input type="checkbox" name="genre" value="thriller" /> <span className={css.checkboxFont}>Thriller</span>
+                                    <input type="checkbox" name="genre" value="animated" /> <span className={css.checkboxFont}>Animated</span>
+                                </div>
+                            </div>
+                            <br />
+                            <div>
+                                <label className={css.labelClass}>Rating</label><br />
+                                <select onClick={this.commonChange}>
+                                    <option value="" defaultChecked>No Rating</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            <br />
+                            <div className={css.searchBtnDiv} onClick={(event) => {
+                                this.submitMovie(event)
+                            }}>
+                                <span>Add movie</span>
+                                <img src="https://firebasestorage.googleapis.com/v0/b/jonathanjohansson-69096.appspot.com/o/popcorn.png?alt=media&token=74049f62-676d-481a-a5c5-afc53e4035f7" alt="popcorn" />
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-            <br />
-            <div>
-                <label className={css.labelClass}>Rating</label><br />
-                <select onClick={this.commonChange}>
-                    <option value="" defaultChecked>No Rating</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                </select>
-            </div>
-            <br />
-            <div className={css.searchBtnDiv} onClick={(event)=> {
-                this.submitMovie(event)
-                }}>
-            <span>Add movie</span>
-           <img  src="https://firebasestorage.googleapis.com/v0/b/jonathanjohansson-69096.appspot.com/o/popcorn.png?alt=media&token=74049f62-676d-481a-a5c5-afc53e4035f7" alt="popcorn"/>
-           </div>
-           
-            {/* <button className={css.addMovieBtn} value="Add movie" onClick={(event)=> {
-                this.submitMovie(event)
-                }}>Add movie</button> */}
-        </div>
-    </div>
-</div>
         )
     }
 }
--- components/movieList.js
+++ components/movieList.js
@@ -1,96 +1,84 @@
 import React from 'react'
 
 class MovieList extends React.Component {
-constructor(props) {
-super(props);
-this.state = {
-    movies: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
     }
-}
+  }
 
-componentDidMount(){
+  componentDidMount() {
 
-  console.log("componentDidMount")
-  fetch("http://localhost:3000/api/")
-  .then(response => response.json())
-  .then(json=> {
+    fetch("http://localhost:3000/api/")
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          movies: json
+        })
+        console.log(json);
+      }
+      )
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  deleteCard(index, data) {
+    let url = "http://localhost:3000/api/delete";
+
+    fetch(url + "?name=" + data.name)
+      .then(response => response.text())
+      .then(text => console.log(text))
+
+    this.setState(prevState => ({
+      movies: [...prevState.movies.slice(0, index), ...prevState.movies.slice(index + 1)]
+    }))
+  }
+
+  newMovies(res) {
+    console.log(res)
     this.setState({
-      movies:json
+      movies: res
     })
-    console.log(json)
+  }
+
+  render() {
+    let movies = [];
+    let movieData = [...this.state.movies];
+
+    movieData.forEach((data) => {
+      movies.push(data);
+    });
+
+    let style = {
+      display: "flex",
+      justifyContent: "center",
     }
-  )
-}
-
-deleteCard(index, data){
-
-  console.log("deleteCard")
-
-  console.log(index);
-  console.log(data.name);
-  console.log(data.year);
-  let url = "http://localhost:3000/api/delete";
-  fetch(url+"?name="+data.name)
-  .then(response => response.text())
-  .then(text => console.log(text))
-
-this.setState(prevState => ({
-    movies: [...prevState.movies.slice(0, index), ...prevState.movies.slice(index + 1)]
-}))
-}
-
-newMovies(res){
-  console.log(res)
-  this.setState({
-    movies:res
-  })
-}
-
-
-componentDidUpdate(){
-
-
-
-}
-
-render() {
-console.log(this.props.movies)
-
-let movies = [];
-let movieData = [...this.state.movies];
-
-movieData.forEach((data)=>{
-    movies.push(data);
-});
-
-let style = {
-  display:"flex",
-  justifyContent:"center",
-}
-return (
-<div style={style}>
-    <h2>Our top movies</h2>
-    <div>
-        <ul>{movies.map((data, index) =>
-        <li key={index}>
-                <span
+    return (
+      <div style={style}>
+        <h2>Our top movies</h2>
+        <div>
+          <ul>{movies.map((data, index) =>
+            <li key={index}>
+              <span
                 onClick={(event) => {
-                    event.stopPropagation();
-                    event.preventDefault();
-                    this.deleteCard(index, data);
+                  event.stopPropagation();
+                  event.preventDefault();
+                  this.deleteCard(index, data);
                 }}>X</span>
-                <h3>{data.name}</h3>
-                <p>Genre: {data.genre}</p>
-                <p>Release year: {data.year}</p>
-                <p><strong>Rating:</strong> {data.rating}</p>
+              <h3>{data.name}</h3>
+              <p>Genre: {data.genre}</p>
+              <p>Release year: {data.year}</p>
+              <p><strong>Rating:</strong> {data.rating}</p>
             </li>)}
-        </ul>
-</div>
+          </ul>
+        </div>
 
 
-</div>
-)
-}
+      </div>
+    )
+  }
 }
 
 export default MovieList
